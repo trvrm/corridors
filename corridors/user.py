3e7c2cf478b13011c5e1a6119a09ddd5c9a9731a
trvrm <trevor@surveymedia.ca>
2018-08-20 20:23:25 -0400

    more black

--- corridors/user.py
+++ corridors/user.py
@@ -1,22 +1,20 @@
 import uuid
+
+
 class User:
-    '''
+    """
         Human player.
-    '''
-    def __init__(self,name):
-        self.uuid=uuid.uuid4()
-        self.name=name
-        
-        
+    """
+
+    def __init__(self, name):
+        self.uuid = uuid.uuid4()
+        self.name = name
+
     def __str__(self):
         return self.name
-        
+
     def __repr__(self):
         return f"User ({self.name}:{self.uuid})"
 
     def __json__(self):
-        return {
-            'name':self.name,
-            'uuid':self.uuid
-        }
-        
+        return {"name": self.name, "uuid": self.uuid}
--- corridors/utilities.py
+++ corridors/utilities.py
@@ -5,22 +5,25 @@ import datetime
 import contextlib
 import logging
 
+
 @contextlib.contextmanager
 def timed():
-    start=datetime.datetime.now()
+    start = datetime.datetime.now()
     try:
-        yield  
+        yield
     finally:
-        duration=datetime.datetime.now()-start
+        duration = datetime.datetime.now() - start
         logging.info(duration)
-        
+
+
 def default_encoder(thing):
-    if isinstance(thing,uuid.UUID):
+    if isinstance(thing, uuid.UUID):
         return str(thing)
-    if hasattr(thing,'__json__'):
+    if hasattr(thing, "__json__"):
         return thing.__json__()
-    raise TypeError(repr(thing) + ' is not JSON serializable')
-
-dumps = functools.partial(json.dumps,indent=0, sort_keys=True,ensure_ascii=False, default=default_encoder)
+    raise TypeError(repr(thing) + " is not JSON serializable")
 
 
+dumps = functools.partial(
+    json.dumps, indent=0, sort_keys=True, ensure_ascii=False, default=default_encoder
+)
